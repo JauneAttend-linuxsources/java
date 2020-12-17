8a60e2fd4c3d6761976e673cb899bb6137ff433a
jonathan <jonathan@jonathan-Lenovo-V110-15ISK>
2020-12-17 19:17:04 +0000

    changes

--- LIBRARY/src/User.java
+++ LIBRARY/src/User.java
@@ -1,4 +1,5 @@
 import java.io.FileNotFoundException;
+import java.time.LocalDate;
 import java.util.*;
 public class User {
 
@@ -6,6 +7,7 @@ public class User {
 	protected String password;
     private ArrayList<Book> BorrowedBook = new ArrayList<Book>();
 	private ArrayList<Book> BoughtBook = new ArrayList<Book>();
+	private Integer balance = 0;
 	
 	public User() {
 		
@@ -36,6 +38,13 @@ public class User {
 		return this.password;
 	}
 	
+	public Integer getBalance() {
+		return this.balance;
+	}
+	
+	public void setBalance(Integer balance) {
+		this.balance = balance;
+	}
 	/**
 	 * 
 	 * @param name | set the name of the user
@@ -58,7 +67,38 @@ public class User {
 	public void printUser() {
 		System.out.println("User " + this.name);
 	}
+	/**
+	 * 
+	 * @return a string containing all the purchases
+	 */
+	public String MyPurchases() {
+		System.out.println("Your purchases :");
+		String str = "Book Price\n";
+		for (int i = 0; i < this.BoughtBook.size();i++) {
+			str = str + BoughtBook.get(i).getName() +" $" + BoughtBook.get(i).getPrice() + "\n" ;
+		}
+		return str;
+	}
 	
+	public String MyDueDates() {
+		System.out.println("Your Due Dates :");
+		String str = "Books Due-Date\n\n";
+		for (int i = 0; i < this.BorrowedBook.size();i++) {
+			str = str + BorrowedBook.get(i).getName() + " " + BorrowedBook.get(i).getDueDate() +  "\n";
+		}
+		return str;
+	}
+
+	/**
+	 * 
+	 * @return true if the purchases section is empty, false otherwise
+ 	 */
+	public Boolean isPurchasesEmpty() {
+		if(this.BoughtBook.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
 	/**
 	 * 
 	 * @param name | name of the user
@@ -69,7 +109,9 @@ public class User {
 		if(lib.getBook(name).getAvailability()) {
 			this.BorrowedBook.add(lib.getBook(name));
 			lib.getBook(name).setAvailable(false);
-			System.out.println("Book sucessfully borrowed...");
+			Receipt r = new Receipt();
+			lib.getBook(name).setDueDate(r.BringBackDate());
+			System.out.println("Book sucessfully borrowed.");
 		}
 		else {
 		
@@ -83,14 +125,26 @@ public class User {
 	 * @param lib  | library where you buy
 	 */
 	public void BuyB(String name, Library lib) throws FileNotFoundException {
+		/*
+		 * If the book is available
+		 * 1- we add it in the purchase list
+		 * 2- we set its availability to false
+		 * 3- we remove the book from the library
+		 * 
+		 *  else we print that the book is not available
+		 */
 		if(lib.getBook(name).getAvailability()) {
+			Book book = lib.getBook(name);
 			this.BoughtBook.add(lib.getBook(name));
-			lib.getBook(name).setAvailable(false);
+			book.setAvailable(false);
+			book.setBoughtDate(LocalDate.now());
 			lib.removeB(name, lib);
-			System.out.println("Book sucessfully bought! you can take out your receipt");
+			Receipt r = new Receipt();
+			r.generateReceiptP(book, this.name);
+			System.out.println("\nBook sucessfully bought! you can take out your receipt");
 		}
 		else {
-			System.out.println("This book is not available");
+			System.out.println("\nThis book is not available");
 		}
 		
 	}
@@ -100,16 +154,18 @@ public class User {
 	 * @param name | name of the book
 	 * @param lib  | library where you search
 	 */
-	public void SearchForBook(String name, Library lib) {
+	public Boolean SearchForBook(String name, Library lib) {
 		if(lib.getBook(name) == null) {
 			System.out.println("this book is not in our database");
+			return false;
 		}
 		else if(lib.getBook(name).getAvailability()) {
 			lib.getBook(name).printBook();
-			System.out.print(" " + "is available");
+			return true;
 		}
 		else {
 			System.out.println("this book has been borrowed or bought");
+			return false;
 		}
 	}
 	
